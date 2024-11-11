0d761a98882d8c667678e6252a917de632aae7b1
Jyoti Prakash <jyoti.prakash@orionesolutions.in>
2024-11-11 18:19:47 +0530

    handling urls in node js

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
 const myServer = http.createServer((req,res) => {
-    console.log("New Request");
-    res.end("hello from server");
+    const log = `${Date.now()}: ${req.url} New req received\n`
+    const myUrl = url.parse(req.url,true);
+    console.log(myUrl);
+    fs.appendFile("log.txt",log,(err,data) => {
+        switch(myUrl.pathname){
+            case "/":
+                res.end("Hii jp homepage");
+                break;
+            case "/about":
+                const qp = myUrl.query.name;
+                res.end(`hi, ${qp}`);
+            default:
+                res.end("404 not found");
+        }
+    });
+
+    
 });
+
+myServer.listen('8000',() => console.log("server started"));
